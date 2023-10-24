e905d2259cb256229f7d879482877a058835b357
oscar blum <oscar.blum@gmail.com>
2023-10-23 22:50:46 -0500

    add crud for dates

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,5 +1,24 @@
+# == Schema Information
+#
+# Table name: quotes
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  company_id :bigint           not null
+#
+# Indexes
+#
+#  index_quotes_on_company_id  (company_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (company_id => companies.id)
+#
 class Quote < ApplicationRecord
   belongs_to :company
+  has_many :line_item_dates, dependent: :destroy
 
   validates :name, presence: true
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'line_item_dates/new'
+  get 'line_item_dates/create'
   root to: "pages#home"
 
   devise_for :users
-  resources :quotes
+
+  resources :quotes do
+    resources :line_item_dates, except: [:index, :show]
+  end
 end
